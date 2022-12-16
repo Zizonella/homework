07178f2b481b06a2feb3e8554476943b55350c79
Zizonella <buzasizita@gmail.com>
2022-12-16 12:26:49 +0000

    Started exercise 3

--- Week2/exercise.cpp
+++ Week2/exercise.cpp
@@ -29,13 +29,6 @@ void Display(char board[3][3])
     }
 }
 
-int main()
-{
-    cout << "this test" << endl;
-    Initialise(board);
-    Display(board);
-}
-
 // Exercise 2
 void SetValue(int row, int col, char symbol, char board[3][3])
 {
@@ -46,3 +39,30 @@ bool IsFree(const int row, const int col, const char board[3][3])
 {
     return (board[row][col] == '-'); // if it is / then it means it is empty i.e. free
 }
+
+int main()
+{
+    int randomNumber = rand();
+    bool gameOver = false;
+
+    int row = 0;
+    int col;
+    char player = PLAYER_1;
+    int count = 0;
+    cout << "this test" << endl;
+
+    Initialise(board);
+    Display(board);
+
+    while (!gameOver)
+    {
+        do
+        {
+            row = rand() % 2 + 1;
+            col = rand() % 2 + 1;
+        }
+        while (!isFree(row, col, board))
+    }
+}
+
+
